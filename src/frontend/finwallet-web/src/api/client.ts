c37ef89b0d2ca53b8505d0f1fa9e4218eb806869
Sami Awadh <eng.sami.cyber@gmail.com>
2026-07-10 19:13:14 +0200

    fix the old back ip

--- src/frontend/finwallet-web/src/api/client.ts
+++ src/frontend/finwallet-web/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const IS_PRODUCTION = true;
 const API_URL = IS_PRODUCTION
-  ? 'http://34.35.119.207:8081'
+  ? 'http://34.35.133.90:8081'
   : 'http://localhost:8081';
 
 const apiClient = axios.create({
